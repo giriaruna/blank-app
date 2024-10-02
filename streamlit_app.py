487a3776dcfc12a26d64f09fb6fe7e99977f7e12
Aruna Giri <112208016+giriaruna@users.noreply.github.com>
2024-10-02 14:06:25 +0000

    change in my code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,52 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+#for displaying images
+from PIL import Image
+import seaborn as sns
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Data Science App")
+
+image_path = Image.open("winebottle.jpeg")
+st.image(image_path, width = 250)
+
+df = pd.read_csv("wine.csv")
+
+st.dataframe(df.head(5))
+
+st.subheader("01. Description of the dataset")
+
+st.dataframe(df.describe())
+
+st.subheader("02. Missing Values")
+
+dfnull = df.isnull()/len(df)*100
+total_missing = dfnull.sum().round(2)
+st.write(total_missing)
+st.write(dfnull)
+
+if total_missing[0] == 0.0:
+    st.success("Congrats you have now missing values")
+
+
+st.subheader("03. Data Visualization")
+
+list_columns = df.columns
+
+values = st.multiselect("Select two variables: ", list_columns, ["quality", "citric acid"])
+
+#Creation of the line chart
+st.line_chart(df, x=values[0], y=values[1])
+
+
+#Creation of the bar chart
+st.bar_chart(df, x=values[0], y=values[1])
+
+
+# Pairplot
+values_pairplot = st.multiselect("Select 4 variables: ", list_columns, ["quality", "citric acid", "alcohol", "chlorides"])
+
+df2 = df[[values_pairplot[0], values_pairplot[1], values_pairplot[2], values_pairplot[3]]]
+pair = sns.pairplot(df2)
+st.pyplot(pair)
+
